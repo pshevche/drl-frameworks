7cabc8295245c4f10a57485b7d805f5efa484c43
pshevche <pavel.shevchenko.95@gmail.com>
2019-04-22 17:01:28 +0200

    Info logging added

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -2,5 +2,6 @@ cd ..
 for fullfile in src/dopamine/experiments/*.gin; do 
     filename=$(basename -- "$fullfile")
     experiment="${filename%.*}"
+    echo "--- STARTING EXPERIMENT ${experiment} --- "
     python src/dopamine/evaluation.py --base_dir="src/dopamine/results/${experiment}" --gin_files="src/dopamine/experiments/${experiment}.gin"
 done
