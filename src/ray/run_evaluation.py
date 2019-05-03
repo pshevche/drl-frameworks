a05a017c33b6bebfede43e57e02c367d2afb167e
pshevche <pavel.shevchenko.95@gmail.com>
2019-05-03 15:07:54 +0200

    simplify ray experiments

--- src/ray/run_evaluation.py
+++ src/ray/run_evaluation.py
@@ -15,6 +15,8 @@ from ray.tune.config_parser import make_parser
 from ray.tune.trial import resources_to_json
 from ray.tune.tune import _make_scheduler, run_experiments
 
+from ray.rllib.train import create_parser
+
 EXAMPLE_USAGE = """
 Training example via RLlib CLI:
     rllib train --run DQN --env CartPole-v0
@@ -27,73 +29,6 @@ Grid search example via executable:
 
 Note that -f overrides all other trial-specific command-line options.
 """
-
-
-def create_parser(parser_creator=None):
-    parser = make_parser(
-        parser_creator=parser_creator,
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        description="Train a reinforcement learning agent.",
-        epilog=EXAMPLE_USAGE)
-
-    # See also the base parser definition in ray/tune/config_parser.py
-    parser.add_argument(
-        "--redis-address",
-        default=None,
-        type=str,
-        help="Connect to an existing Ray cluster at this address instead "
-        "of starting a new one.")
-    parser.add_argument(
-        "--ray-num-cpus",
-        default=None,
-        type=int,
-        help="--num-cpus to use if starting a new cluster.")
-    parser.add_argument(
-        "--ray-num-gpus",
-        default=None,
-        type=int,
-        help="--num-gpus to use if starting a new cluster.")
-    parser.add_argument(
-        "--ray-num-nodes",
-        default=None,
-        type=int,
-        help="Emulate multiple cluster nodes for debugging.")
-    parser.add_argument(
-        "--ray-redis-max-memory",
-        default=None,
-        type=int,
-        help="--redis-max-memory to use if starting a new cluster.")
-    parser.add_argument(
-        "--ray-object-store-memory",
-        default=None,
-        type=int,
-        help="--object-store-memory to use if starting a new cluster.")
-    parser.add_argument(
-        "--experiment-name",
-        default="default",
-        type=str,
-        help="Name of the subdirectory under `local_dir` to put results in.")
-    parser.add_argument(
-        "--resume",
-        action="store_true",
-        help="Whether to attempt to resume previous Tune experiments.")
-    parser.add_argument(
-        "--env", default=None, type=str, help="The gym environment to use.")
-    parser.add_argument(
-        "--queue-trials",
-        action='store_true',
-        help=(
-            "Whether to queue trials when the cluster does not currently have "
-            "enough resources to launch one. This should be set to True when "
-            "running on an autoscaling cluster to enable automatic scale-up."))
-    parser.add_argument(
-        "-f",
-        "--config-file",
-        default=None,
-        type=str,
-        help="If specified, use config options from this file. Note that this "
-        "overrides any trial-specific options set via flags above.")
-    return parser
 
 
 def run(args, parser):
--- src/ray/run_propagation_test.py
+++ src/ray/run_propagation_test.py
@@ -14,17 +14,13 @@ import gym
 import ray
 from ray.rllib.agents.registry import get_agent_class
 from ray.rllib.evaluation.sample_batch import DEFAULT_POLICY_ID
+from ray.rllib.rollout import rollout
 
 
 EXAMPLE_USAGE = """
 Example Usage:
     python src/ray/run_propagation_test.py --checkpoint=src/ray/results/dqn_cartpole_cpu_1/dqn_cartpole_cpu_1/DQN_CartPole-v0_0_2019-05-03_13-40-44mqad943p/src/ray/results/dqn_cartpole_cpu_1/dqn_cartpole_cpu_1/DQN_CartPole-v0_0_2019-05-03_13-40-44mqad943p/checkpoint_1/checkpoint-1 -f=src/ray/experiments/cartpole/dqn_cartpole_cpu_1.yml --steps=500000
 """
-
-# Note: if you use any custom models or envs, register them here first, e.g.:
-#
-# ModelCatalog.register_custom_model("pa_model", ParametricActionsModel)
-# register_env("pa_cartpole", lambda _: ParametricActionCartpole(10))
 
 
 def create_parser(parser_creator=None):
@@ -80,79 +76,6 @@ def run(args, parser):
             str(end_time - start_time) + ' seconds for propagation.')
     f.close()
     print("--- RAY CARTPOLE PROPAGATION EXPERIMENT COMPLETED ---\n")
-
-
-def rollout(agent, env_name, num_steps, out=None, no_render=True):
-    if hasattr(agent, "local_evaluator"):
-        env = agent.local_evaluator.env
-        multiagent = agent.local_evaluator.multiagent
-        if multiagent:
-            policy_agent_mapping = agent.config["multiagent"][
-                "policy_mapping_fn"]
-            mapping_cache = {}
-        policy_map = agent.local_evaluator.policy_map
-        state_init = {p: m.get_initial_state() for p, m in policy_map.items()}
-        use_lstm = {p: len(s) > 0 for p, s in state_init.items()}
-    else:
-        env = gym.make(env_name)
-        multiagent = False
-        use_lstm = {DEFAULT_POLICY_ID: False}
-
-    if out is not None:
-        rollouts = []
-    steps = 0
-    while steps < (num_steps or steps + 1):
-        if out is not None:
-            rollout = []
-        state = env.reset()
-        done = False
-        reward_total = 0.0
-        while not done and steps < (num_steps or steps + 1):
-            if multiagent:
-                action_dict = {}
-                for agent_id in state.keys():
-                    a_state = state[agent_id]
-                    if a_state is not None:
-                        policy_id = mapping_cache.setdefault(
-                            agent_id, policy_agent_mapping(agent_id))
-                        p_use_lstm = use_lstm[policy_id]
-                        if p_use_lstm:
-                            a_action, p_state_init, _ = agent.compute_action(
-                                a_state,
-                                state=state_init[policy_id],
-                                policy_id=policy_id)
-                            state_init[policy_id] = p_state_init
-                        else:
-                            a_action = agent.compute_action(
-                                a_state, policy_id=policy_id)
-                        action_dict[agent_id] = a_action
-                action = action_dict
-            else:
-                if use_lstm[DEFAULT_POLICY_ID]:
-                    action, state_init, _ = agent.compute_action(
-                        state, state=state_init)
-                else:
-                    action = agent.compute_action(state)
-
-            next_state, reward, done, _ = env.step(action)
-
-            if multiagent:
-                done = done["__all__"]
-                reward_total += sum(reward.values())
-            else:
-                reward_total += reward
-            if not no_render:
-                env.render()
-            if out is not None:
-                rollout.append([state, action, next_state, reward, done])
-            steps += 1
-            state = next_state
-        if out is not None:
-            rollouts.append(rollout)
-        print("Episode reward", reward_total)
-
-    if out is not None:
-        pickle.dump(rollouts, open(out, "wb"))
 
 
 if __name__ == "__main__":
