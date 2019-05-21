649debcbdcafc26b631d0f488ae644877e29fcb7
pshevche <pavel.shevchenko.95@gmail.com>
2019-05-21 13:24:24 +0200

    install dopamine and ray from pypi

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,9 +5,6 @@ source ~/miniconda3/etc/profile.d/conda.sh
 # Create environments
 echo "--- SETTING UP DOPAMINE ENVIRONMENT ---"
 conda env create -f config/dopamine.yml
-conda activate dopamine-env
-pip install lib/dopamine_rl-2.0.3.tar.gz
-conda deactivate
 echo "--- SETTING UP HORIZON ENVIRONMENT ---"
 conda env create -f config/horizon.yml
 conda activate horizon-env
@@ -15,7 +12,4 @@ pip install lib/horizon-0.1.tar.gz
 conda deactivate
 echo "--- SETTING UP RAY ENVIRONMENT ---"
 conda env create -f config/ray.yml
-conda activate ray-env
-pip install lib/ray-0.7.0-cp36-cp36m-manylinux1_x86_64.whl
-conda deactivate
 echo "--- SETUP COMPLETED ---"
