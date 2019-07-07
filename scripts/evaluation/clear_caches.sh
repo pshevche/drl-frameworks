74a2e0c08768e01ecae62cf7abd36c6dda8ecc70
pshevche <pavel.shevchenko.95@gmail.com>
2019-07-07 21:16:41 +0200

    fixed tests in docker

--- scripts/evaluation/clear_caches.sh
+++ scripts/evaluation/clear_caches.sh
@@ -1,6 +1,2 @@
-# remove dopamine cache
-find drl_fw/dopamine -name __pycache__ -type d -exec rm -r {} +
-# remove horizon cache
-find drl_fw/horizon -name __pycache__ -type d -exec rm -r {} +
-# remove ray cache
-find drl_fw/ray -name __pycache__ -type d -exec rm -r {} +
+# remove caches
+find ./ -name __pycache__ -type d -exec rm -r {} +
