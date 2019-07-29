9fe05584654666e984b2ff3c733b6f61d69a45ee
pshevche <pavel.shevchenko.95@gmail.com>
2019-07-29 12:21:12 +0200

    add pytest timeout

--- scripts/setup_local_env.sh
+++ scripts/setup_local_env.sh
@@ -12,7 +12,7 @@ pip install -e src/
 pip install zmq psycopg2-binary networkx wget
 
 # Setup Postgres container
-docker build -t pg park/query-optimizer/docker/
+docker build -t pg src/park/query-optimizer/docker/
 docker start docker-pg || docker run --name docker-pg -p 0.0.0.0:5432:5432 --net drl-net --privileged -d pg
 
 conda deactivate
